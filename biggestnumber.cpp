84ad41808c32ed4d8feba34e6f25c5b29d6e3693
akaldhaheri <akaldhaheri@mail.csuchico.edu>
2018-02-06 11:50:23 -0800

    Learned how to make the compter know to find the biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,17 +1,32 @@
-//Author:
+//Author: Abdulla Aldhaheri
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+	int whole_number_1, whole_number_2, biggest_number;
+
+	cout<<"Please enter a whole number:\n";
+	cin>> whole_number_1;
+
+	cout<<"Please enter another whole number:\n";
+	cin>> whole_number_2;
+
+	if(whole_number_1 > whole_number_2)
+	{
+		biggest_number=whole_number_1;
+
+	}
+	else{
+		biggest_number+whole_number_2;
+
+	}
+
+	cout<<"Of those two numbers, the biggest is:\n"<<biggest_number;
+
+	cout<<endl<<"Thank you for playing.\n";
 
   return 0;
 }
